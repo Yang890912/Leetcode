62461c1249d6c71201eb0db947b8a275bf22a57b
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2025-07-17 22:25:53 +0800

    Time: 18 ms (84.42%), Space: 18.6 MB (16.01%), Date: 07-17-2025

---
+++ LeetCode/Medium/0018-4sum/0018-4sum.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        set<vector<int>> us;
+
+        if (nums.size() < 4) return res;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            for (int j = i + 3; j < nums.size(); j++) {
+                int left = i + 1;
+                int right = j - 1;
+                long long newTarget = (long long) target - nums[i] - nums[j];
+
+                while (left < right) {
+                    if (nums[left] + nums[right] > newTarget) right--;
+                    else if (nums[left] + nums[right] < newTarget) left++;
+                    else {
+                        us.insert({nums[i], nums[left], nums[right], nums[j]});
+                        left++;
+                        right--;
+                    }
+                }
+            }
+        }
+
+        for (const auto& vec : us) {
+            res.push_back(vec);
+        }
+        return res;
+    }
+};

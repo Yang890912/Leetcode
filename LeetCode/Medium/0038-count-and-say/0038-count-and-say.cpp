a18e7632c92bff4928dc11d65faccf62d866aa6b
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2025-07-18 11:44:51 +0800

    Time: 6 ms (29.78%), Space: 10.2 MB (29.54%), Date: 07-18-2025

---
+++ LeetCode/Medium/0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        if (n == 1) return "1";
+        return transStr(countAndSay(n - 1));
+    }
+
+    string transStr(string s) {
+        string output = "";
+        int freq = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (i == 0 || s[i - 1] == s[i]) freq++;
+            else {
+                output += to_string(freq);
+                output += to_string(s[i - 1] - '0');
+                freq = 1;
+            }
+        }
+        output += to_string(freq);
+        output += to_string(s[s.size() - 1] - '0');
+        return output;
+    }   
+};

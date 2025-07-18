a3bc833acff1292e82f9d7fd6d0f0647006bd9de
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2025-07-18 11:48:22 +0800

    Time: 0 ms (100%), Space: 112.3 MB (72.16%), Date: 07-18-2025

---
+++ LeetCode/Medium/0134-gas-station/0134-gas-station.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int start = 0;
+        int currPos = start;
+        int currGas = 0;
+        int remainCost = 0;
+
+        while (start < gas.size()) {
+            currGas += gas[currPos];
+            currGas -= cost[currPos];
+
+            if (currPos == gas.size() - 1) {
+                if (currGas - remainCost < 0) return -1;
+                else return start;
+            }
+
+            if (currGas < 0) {
+                remainCost -= currGas;
+                currGas = 0;
+                start = currPos + 1;
+                currPos = start;
+                continue;
+            }
+
+            currPos++;
+        }
+
+        return -1;
+    }
+};

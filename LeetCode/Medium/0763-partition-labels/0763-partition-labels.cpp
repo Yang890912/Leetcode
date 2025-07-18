3c52010da92b31be58006f5d2ef37c98c7882730
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2025-07-18 13:13:12 +0800

    Time: 0 ms (100%), Space: 9 MB (47.2%), Date: 07-18-2025

---
+++ LeetCode/Medium/0763-partition-labels/0763-partition-labels.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        vector<int> last(26, 0);
+        vector<int> res;
+
+        for (int i = 0; i < s.size(); i++) {
+            last[s[i] - 'a'] = i;
+        }
+
+        int start = 0;
+        while (start < s.size()) {
+            int currLast = last[s[start] - 'a'];
+            for (int j = start; j <= currLast; j++) {
+                if (last[s[j] - 'a'] > currLast) currLast = last[s[j] - 'a'];
+            }
+            res.push_back(currLast - start + 1);
+            start = currLast + 1;
+        }
+
+        return res;
+    }
+};

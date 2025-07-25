638115673564c9645b9be90ed2d3b0e65426751e
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2025-07-25 13:40:26 +0800

    Time: 2 ms (30.41%), Space: 29.7 MB (37.84%), Date: 07-25-2025

--- LeetCode/Easy/3487-maximum-unique-subarray-sum-after-deletion/3487-maximum-unique-subarray-sum-after-deletion.cpp
+++ LeetCode/Easy/3487-maximum-unique-subarray-sum-after-deletion/3487-maximum-unique-subarray-sum-after-deletion.cpp
@@ -3,22 +3,22 @@ public:
     int maxSum(vector<int>& nums) {
         int res = INT_MIN;
 
-        vector<int> newNums;
-        unordered_set<int> us;
-        for (int num : nums) {
-            if (us.count(num) == 0) {
-                us.insert(num);
-                newNums.push_back(num);
-            }
-        }
-
         int currSum = 0;
-        for (int i = 0; i < newNums.size(); i++) {
-            if (i == 0) currSum = newNums[i];
-            else if (newNums[i] < 0 && newNums[i] > currSum) currSum = newNums[i];
-            else if (newNums[i] >= 0 && currSum < 0) currSum = newNums[i];
-            else if (newNums[i] >= 0) currSum += newNums[i];
-            
+        unordered_map<int, int> um;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i == 0) {
+                currSum = nums[i];
+                um[nums[i]] = 1;
+            }
+            else if (um.count(nums[i]) > 0) continue;
+            else {
+                if (currSum >= 0 && nums[i] >= 0) currSum += nums[i];
+                else if (currSum < 0) {
+                    um.clear();
+                    currSum = nums[i];
+                }
+                um[nums[i]] = 1;
+            }
             res = max(res, currSum);
         }
 

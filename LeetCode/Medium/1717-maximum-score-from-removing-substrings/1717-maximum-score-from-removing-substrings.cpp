229ba83d67d4c2c58694e5a6bf4608583af1e109
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2025-07-25 14:34:42 +0800

    Time: 57 ms (35.32%), Space: 24.7 MB (61.34%), Date: 07-25-2025

--- LeetCode/Medium/1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.cpp
+++ LeetCode/Medium/1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.cpp
@@ -1,51 +1,36 @@
 class Solution {
 public:
-    int maximumGain(string s, int x, int y) {
-        int maxGain = 0;
-
-        stack<char> myStack;
+    int remove(string& s, string pattern, int score) {
+        int gain = 0;
+        stack<char> st;
         for (int i = 0; i < s.size(); i++) {
-            if (s[i] != 'a' && s[i] != 'b') {
-                stack<char> myStack2;
-                while (!myStack.empty()) {
-                    if (myStack2.empty()) myStack2.push(myStack.top()); 
-                    else if (myStack.top() == myStack2.top()) myStack2.push(myStack.top());
-                    else {
-                        if (myStack.top() == 'a') maxGain += x;
-                        else maxGain += y;
-                        myStack2.pop();
-                    }
-                    myStack.pop();
-                }
-            }
-            else if (myStack.empty()) myStack.push(s[i]);
-            else if (myStack.top() == s[i]) myStack.push(s[i]);
-            else if (x > y) {
-                if (s[i] == 'a') myStack.push(s[i]);
-                else {
-                    maxGain += x;
-                    myStack.pop();
-                }
-            }
-            else if (y >= x) {
-                if (s[i] == 'b') myStack.push(s[i]);
-                else {
-                    maxGain += y;
-                    myStack.pop();
-                }
+            if (st.empty()) st.push(s[i]);
+            else if (st.top() != pattern[0] || s[i] != pattern[1]) st.push(s[i]);
+            else {
+                gain += score;
+                st.pop();
             }
         }
 
-        stack<char> myStack2;
-        while (!myStack.empty()) {
-            if (myStack2.empty()) myStack2.push(myStack.top()); 
-            else if (myStack.top() == myStack2.top()) myStack2.push(myStack.top());
-            else {
-                if (myStack.top() == 'a') maxGain += x;
-                else maxGain += y;
-                myStack2.pop();
-            }
-            myStack.pop();
+        s.clear();
+        while (!st.empty()) {
+            s += st.top();
+            st.pop();
+        }
+        reverse(s.begin(), s.end());
+
+        return gain;
+    }
+
+    int maximumGain(string s, int x, int y) {
+        int maxGain = 0;
+        if (x > y) {
+            maxGain += remove(s, "ab", x);
+            maxGain += remove(s, "ba", y);
+        }
+        else {
+            maxGain += remove(s, "ba", y);
+            maxGain += remove(s, "ab", x);
         }
 
         return maxGain;

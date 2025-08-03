68195c2b9c773f8b82dc83efa910f2752435f578
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2025-08-03 17:15:43 +0800

    Time: 2 ms (15.03%), Space: 9.8 MB (11.17%), Date: 08-03-2025

---
+++ LeetCode/Easy/0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res;
+
+        for (int i = 0; i < numRows; i++) {
+            if (i == 0) res.push_back({1});
+            else if (i == 1) res.push_back({1, 1});
+            else {
+                vector<int> row;
+                int n = res[i - 1].size();
+                row.push_back(1);
+                for (int j = 1; j < n; j++) {
+                    row.push_back(res[i - 1][j - 1] + res[i - 1][j]);
+                }
+                row.push_back(1);
+                res.push_back(row);
+            }
+        }
+
+        return res;
+    }
+};

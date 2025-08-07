1cb86957ad8d0c93e55c606b40954da349158934
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2025-08-08 00:12:48 +0800

    Time: N/A (0%), Space: N/A (0%), Date: 08-08-2025

---
+++ LeetCode/Medium/3341-find-minimum-time-to-reach-last-room-i/3341-find-minimum-time-to-reach-last-room-i.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+        int n = moveTime.size();
+        int m = moveTime[0].size();
+        vector<vector<int>> dist(n, vector<int>(m, INT_MAX));
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> pq;
+
+        moveTime[0][0] = 0;
+        dist[0][0] = 0;
+        pq.push({dist[0][0], 0, 0});
+        while (!pq.empty()) {
+            auto [time, x, y] = pq.top(); 
+            pq.pop();
+
+            if (time > dist[x][y]) continue;
+            if (x == n - 1 && y == m - 1) continue;
+
+            vector<vector<int>> DIR = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+            for (auto d : DIR) {
+                int nx = x + d[0];
+                int ny = y + d[1];
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m && dist[nx][ny] > dist[x][y] + moveTime[x][y] + 1) {
+                    dist[nx][ny] = dist[x][y] + moveTime[x][y] + 1;
+                    pq.push({dist[nx][ny], nx, ny});
+                }
+            }
+        }
+
+        return dist[n - 1][m - 1];
+    }
+};

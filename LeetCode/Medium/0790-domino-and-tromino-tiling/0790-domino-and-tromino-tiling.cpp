5ff428dcd34653b3268df239f9b2eec5692191c3
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2025-08-08 16:03:25 +0800

    Time: 0 ms (100%), Space: 9 MB (58.38%), Date: 08-08-2025

--- LeetCode/Medium/0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
+++ LeetCode/Medium/0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -5,14 +5,14 @@ public:
         else if (n == 2) return 2;
         else if (n == 3) return 5;
 
-        int mod = 1e9 + 7;
+        int MOD = 1e9 + 7;
         vector<long long> dp(n + 1, 0);
         dp[1] = 1;
         dp[2] = 2;
         dp[3] = 5;
 
         for (int i = 4; i <= n; i++) {
-            dp[i] = ((2 * dp[i - 1]) % mod + dp[i - 3]) % mod;
+            dp[i] = ((2 * dp[i - 1]) % MOD + dp[i - 3]) % MOD;
         }
 
         return dp[n];

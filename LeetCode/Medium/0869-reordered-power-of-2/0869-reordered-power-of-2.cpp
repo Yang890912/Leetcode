a99411d60b661f8666732fd37439a03a29e3a299
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2025-08-10 11:59:28 +0800

    Time: 42 ms (9.14%), Space: 9.1 MB (51.03%), Date: 08-10-2025

---
+++ LeetCode/Medium/0869-reordered-power-of-2/0869-reordered-power-of-2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool reorderedPowerOf2(int n) {
+        string s = to_string(n);
+
+        sort(s.begin(), s.end());
+        do {
+            if (s[0] == '0') continue;
+            bitset<32> b(stoi(s));
+            if (b.count() == 1) return true;
+        } while(next_permutation(s.begin(), s.end()));
+
+        return false;
+    }
+};

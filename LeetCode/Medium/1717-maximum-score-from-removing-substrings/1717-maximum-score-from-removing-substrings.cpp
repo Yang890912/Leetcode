b18fcdec4966ea664fbdcd86e27f0e69be5b2ecf
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2025-08-15 17:37:41 +0800

    Time: 30 ms (68.15%), Space: 32.3 MB (11.35%), Date: 08-15-2025

--- LeetCode/Medium/1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.cpp
+++ LeetCode/Medium/1717-maximum-score-from-removing-substrings/1717-maximum-score-from-removing-substrings.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
-    int remove(string& s, string pattern, int score) {
+    int maximumGain(string s, int x, int y) {
         int gain = 0;
-        stack<char> st;
-        for (int i = 0; i < s.size(); i++) {
-            if (st.empty()) st.push(s[i]);
-            else if (st.top() != pattern[0] || s[i] != pattern[1]) st.push(s[i]);
-            else {
-                gain += score;
-                st.pop();
-            }
-        }
 
-        s.clear();
-        while (!st.empty()) {
-            s += st.top();
-            st.pop();
+        if (x >= y) {
+            s = calGain(s, gain, x, "ab");
+            calGain(s, gain, y, "ba");
+        } 
+        else {
+            s = calGain(s, gain, y, "ba");
+            calGain(s, gain, x, "ab");
         }
-        reverse(s.begin(), s.end());
 
         return gain;
     }
 
-    int maximumGain(string s, int x, int y) {
-        int maxGain = 0;
-        if (x > y) {
-            maxGain += remove(s, "ab", x);
-            maxGain += remove(s, "ba", y);
-        }
-        else {
-            maxGain += remove(s, "ba", y);
-            maxGain += remove(s, "ab", x);
+    string calGain(string s, int& gain, int canGain, string pattern) {
+        string news = "";
+        for (int i = 0; i < s.size(); i++) {
+            if (news == "") news += s[i];
+            else if (s[i] == pattern[1] && news.back() == pattern[0]) {
+                gain += canGain;
+                news.pop_back();
+            }
+            else news += s[i];
         }
 
-        return maxGain;
+        return news;
     }
 };

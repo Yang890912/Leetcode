f473f93d02b4aad1b724c332f31a8c2649eaccc4
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2025-08-16 20:57:07 +0800

    Time: 87 ms (9.97%), Space: 159.6 MB (7.39%), Date: 08-16-2025

--- LeetCode/Medium/3440-reschedule-meetings-for-maximum-free-time-ii/3440-reschedule-meetings-for-maximum-free-time-ii.cpp
+++ LeetCode/Medium/3440-reschedule-meetings-for-maximum-free-time-ii/3440-reschedule-meetings-for-maximum-free-time-ii.cpp
@@ -1,29 +1,37 @@
 class Solution {
 public:
     int maxFreeTime(int eventTime, vector<int>& startTime, vector<int>& endTime) {
-        int maxFree = 0;
         int n = startTime.size();
-        vector<int> gap(1, startTime[0]);
-
-        for (int i = 1; i < n; i++) {
-            gap.push_back(startTime[i] - endTime[i - 1]);
-        }
-        gap.push_back(eventTime - endTime[n - 1]);
-
-        vector<int> maxRightFree(n + 1, 0);
-        int currMax = 0;
-        for (int i = n - 1; i >= 1; i--) {
-            currMax = max(gap[i + 1], currMax);
-            maxRightFree[i] = currMax;
-        }
-
-        int maxLeftFree = 0;
+        vector<int> gaps;
+        vector<pair<int, int>> sortedGaps;
+        int j = 0;
         for (int i = 0; i < n; i++) {
-            if (i >= 1) maxLeftFree = max(maxLeftFree, gap[i - 1]);
-            
-            int dur = endTime[i] - startTime[i];
-            if (dur <= maxLeftFree || dur <= maxRightFree[i + 1]) maxFree = max(maxFree, gap[i] + gap[i + 1] + dur);
-            else maxFree = max(maxFree, gap[i] + gap[i + 1]);
+            if (i == 0) {
+                gaps.push_back(startTime[i]);
+                sortedGaps.push_back({startTime[i], j++});
+            }
+            else {
+                gaps.push_back(startTime[i] - endTime[i - 1]);
+                sortedGaps.push_back({startTime[i] - endTime[i - 1], j++});
+            }
+                 
+            if (i == n - 1) {
+                gaps.push_back(eventTime - endTime[i]);
+                sortedGaps.push_back({eventTime - endTime[i], j++});
+            }
+        }
+        sort(sortedGaps.rbegin(), sortedGaps.rend());
+
+        int maxFree = 0;
+        for (int i = 0; i < n; i++) {
+            int free = 0;
+            for (int k = 0; k < 3; k++) {
+                if (sortedGaps[k].second != i && sortedGaps[k].second != i + 1 && endTime[i] - startTime[i] <= sortedGaps[k].first) {
+                    free = endTime[i] - startTime[i];
+                    break;
+                }
+            }
+            maxFree = max(maxFree, free + gaps[i] + gaps[i + 1]);
         }
 
         return maxFree;

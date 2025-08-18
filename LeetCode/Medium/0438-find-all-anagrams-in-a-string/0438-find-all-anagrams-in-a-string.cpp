3bead8d5d1ed5f8e1d7807ec8f9d4b4f87868168
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2025-08-18 17:40:53 +0800

    Time: 3 ms (81.87%), Space: 11.5 MB (64.29%), Date: 08-18-2025

---
+++ LeetCode/Medium/0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> res;
+        vector<int> inp('z' + 1, 0);
+        for (char c : p) inp[c]++;
+
+        int left = 0, right = 0;
+        while (left < s.size() && right < s.size()) {
+            if (inp[s[right]] == 0) {
+                inp[s[left]]++;
+                left++;
+                continue;
+            }
+
+            inp[s[right]]--;
+
+            if (right - left == p.size() - 1) {
+                res.push_back(left);
+                inp[s[left]]++;
+                left++;
+            }
+            
+            right++;
+        }
+
+        return res;
+    }
+};

2a91b951f517d1c88c234059499f332b6b702502
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2025-08-26 12:29:32 +0800

    Time: 3 ms (19.83%), Space: 33.7 MB (5.1%), Date: 08-26-2025

---
+++ LeetCode/Easy/3000-maximum-area-of-longest-diagonal-rectangle/3000-maximum-area-of-longest-diagonal-rectangle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int areaOfMaxDiagonal(vector<vector<int>>& dimensions) {
+        vector<vector<double>> diagAndArea(dimensions.size());
+        
+        for (int i = 0; i < dimensions.size(); i++) {
+            diagAndArea[i].push_back(sqrt(dimensions[i][0] * dimensions[i][0] + 
+                                        dimensions[i][1] * dimensions[i][1]));
+            diagAndArea[i].push_back(dimensions[i][0] * dimensions[i][1]);
+        }
+
+        sort(diagAndArea.rbegin(), diagAndArea.rend());
+
+        return diagAndArea[0][1];
+    }
+};

5008f491dd67d642deb6502f4a59d917e39bfb65
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2025-10-10 22:27:56 +0800

    Time: 58 ms (6.03%), Space: 31.6 MB (7.72%), Date: 10-10-2025

--- LeetCode/Medium/0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ LeetCode/Medium/0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,51 +1,39 @@
 class Solution {
 public:
+    vector<vector<int>> DIR = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         int m = heights.size();
         int n = heights[0].size();
+
         vector<vector<bool>> canFlowToPacific(m, vector<bool>(n, false));
         vector<vector<bool>> canFlowToAtlantic(m, vector<bool>(n, false));
-       
-        bool updated = true;
-        while (updated) {
-            updated = false;
-            for (int i = 0; i < m; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (!canFlowToPacific[i][j]) {
-                        if (i == 0) canFlowToPacific[i][j] = true, updated = true;
-                        if (j == 0) canFlowToPacific[i][j] = true, updated = true;
-                        if ((i - 1 >= 0 && heights[i - 1][j] <= heights[i][j] && canFlowToPacific[i - 1][j]) ||
-                            (j - 1 >= 0 && heights[i][j - 1] <= heights[i][j] && canFlowToPacific[i][j - 1]) ||
-                            (i + 1 < m && heights[i + 1][j] <= heights[i][j] && canFlowToPacific[i + 1][j]) ||
-                            (j + 1 < n &&heights[i][j + 1] <= heights[i][j] && canFlowToPacific[i][j + 1])) {
-                            canFlowToPacific[i][j] = true;
-                            updated = true;
-                        }
-                    }
 
-                    if (!canFlowToAtlantic[i][j]) {
-                        if (i == m - 1) canFlowToAtlantic[i][j] = true, updated = true;
-                        if (j == n - 1) canFlowToAtlantic[i][j] = true, updated = true;
-                        if ((i - 1 >= 0 && heights[i - 1][j] <= heights[i][j] && canFlowToAtlantic[i - 1][j]) ||
-                            (j - 1 >= 0 && heights[i][j - 1] <= heights[i][j] && canFlowToAtlantic[i][j - 1]) ||
-                            (i + 1 < m && heights[i + 1][j] <= heights[i][j] && canFlowToAtlantic[i + 1][j]) ||
-                            (j + 1 < n &&heights[i][j + 1] <= heights[i][j] && canFlowToAtlantic[i][j + 1])) {
-                            canFlowToAtlantic[i][j] = true;
-                            updated = true;
-                        }
-                    }
-                }
-            }
-        }
+        for (int i = 0; i < m; i++) dfs(i, 0, canFlowToPacific, heights);
+        for (int j = 0; j < n; j++) dfs(0, j, canFlowToPacific, heights);
+        for (int i = 0; i < m; i++) dfs(i, n - 1, canFlowToAtlantic, heights);
+        for (int j = 0; j < n; j++) dfs(m - 1, j, canFlowToAtlantic, heights);
 
         vector<vector<int>> res;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (canFlowToPacific[i][j] && canFlowToAtlantic[i][j])
-                    res.push_back({i, j});
+                if (canFlowToPacific[i][j] && canFlowToAtlantic[i][j]) res.push_back({i, j});
             }
         }
 
         return res;
+    }
+
+    void dfs(int x, int y, vector<vector<bool>>& flow, vector<vector<int>>& heights) {
+        flow[x][y] = true;
+        for (auto d : DIR) {
+            int nx = x + d[0];
+            int ny = y + d[1];
+
+            if (nx < 0 || nx >= heights.size()) continue;
+            if (ny < 0 || ny >= heights[0].size()) continue;
+            if (flow[nx][ny]) continue;
+            if (heights[nx][ny] >= heights[x][y]) dfs(nx, ny, flow, heights);
+        }
     }
 };

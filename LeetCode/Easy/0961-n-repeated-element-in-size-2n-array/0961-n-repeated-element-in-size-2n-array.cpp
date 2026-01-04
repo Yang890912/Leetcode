269339c3cc53aad0d5024dd018ec5cb542841c57
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2026-01-04 14:14:08 +0800

    Time: 0 ms (100%), Space: 28.4 MB (64.54%), Date: 01-04-2026

--- LeetCode/Easy/0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
+++ LeetCode/Easy/0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -2,10 +2,11 @@ class Solution {
 public:
     int repeatedNTimes(vector<int>& nums) {
         int n = nums.size();
-        sort(nums.begin(), nums.end());
 
-        if (nums[0] == nums[n / 2 - 1]) return nums[0];
+        for (int i = 0; i < n - 2; i++) {
+            if (nums[i] == nums[i + 1] || nums[i] == nums[i + 2]) return nums[i];
+        }
 
-        return nums[n / 2];
+        return nums[n - 1];
     }
 };

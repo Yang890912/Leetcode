84c674842980982c23faa57aefd6066928216a83
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2026-01-21 21:58:16 +0800

    Time: 12 ms (16.9%), Space: 13.2 MB (77.88%), Date: 01-21-2026

--- LeetCode/Medium/2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ LeetCode/Medium/2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int bestClosingTime(string customers) {
+        int n = customers.size();
         int penalty = 0;
         for (char c : customers) {
             if (c == 'N') penalty++;
         }
 
-        int res = customers.size();
+        int res = n;
         int minPenalty = penalty;
-        for (int i = customers.size() - 1; i >= 0; i--) {
+        for (int i = n - 1; i >= 0; i--) {
             if (customers[i] == 'Y') penalty++;
             else penalty--;
 

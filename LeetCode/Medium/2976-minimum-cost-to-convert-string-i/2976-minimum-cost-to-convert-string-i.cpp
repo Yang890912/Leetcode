5e7385000be4010f2fa7924ac7bf1ccf3271d3fe
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2026-01-29 14:58:23 +0800

    Time: 51 ms (39.72%), Space: 100.2 MB (70.9%), Date: 01-29-2026

--- LeetCode/Medium/2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
+++ LeetCode/Medium/2976-minimum-cost-to-convert-string-i/2976-minimum-cost-to-convert-string-i.cpp
@@ -1,69 +1,36 @@
 class Solution {
 public:
-    const int m = 'a' + 26;
-    const long long LLMAX = 1e12;
+    const long long INF = 1e12;
 
     long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
         int n = original.size();
-
-        vector<vector<long long>> adj(m, vector<long long>(m, LLMAX));
-        vector<vector<long long>> dis(m, vector<long long>(m, LLMAX));
-        vector<vector<bool>> found(m, vector<bool>(m, false));
+        vector<vector<long long>> dist(26, vector<long long>(26, INF));
 
         for (int i = 0; i < n; i++) {
-            adj[original[i]][changed[i]] = min((long long)cost[i], adj[original[i]][changed[i]]);
+            int u = original[i] - 'a';
+            int v = changed[i] - 'a';
+            dist[u][v] = min((long long)cost[i], dist[u][v]);
+        }
+
+        for (int k = 0; k < 26; k++) {
+            for (int i = 0; i < 26; i++) {
+                for (int j = 0; j < 26; j++) {
+                    if (dist[i][j] > dist[i][k] + dist[k][j])
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
         }
 
         long long res = 0;
         for (int i = 0; i < source.size(); i++) {
-            int src = source[i];
-            int dst = target[i];
+            int s = source[i] - 'a';
+            int t = target[i] - 'a';
 
-            if (src == dst) continue;
-            else if (found[src][dst]) {
-                if (dis[src][dst] == LLMAX) return -1;
-                res += dis[src][dst];
-            }
-            else {
-                findShortestPath(src, dst, adj, dis, found);
-                if (dis[src][dst] == LLMAX) return -1;
-                res += dis[src][dst];
-            }
+            if (s == t) continue;
+            else if (dist[s][t] == INF) return -1;
+            else res += dist[s][t];
         }
 
         return res;
-    }
-
-    void findShortestPath(  int src, int dst, 
-                            vector<vector<long long>>& adj, 
-                            vector<vector<long long>>& dis,
-                            vector<vector<bool>>& found) {
-
-        vector<bool> used(m, false);
-
-        dis[src][src] = 0;
-        for (int i = 'a'; i <= 'z'; i++) {
-            long long currMin = LLMAX;
-            int picked = -1;
-            
-            for (int j = 'a'; j <= 'z'; j++) {
-                if (used[j]) continue;
-
-                if (dis[src][j] < currMin) {
-                    picked = j;
-                    currMin = dis[src][j];
-                }
-            }
-
-            if (picked == -1) break;
-            used[picked] = true;
-
-            for (int v = 'a'; v <= 'z'; v++) {
-                if (dis[src][v] > dis[src][picked] + adj[picked][v])
-                    dis[src][v] = dis[src][picked] + adj[picked][v];
-            }
-        }
-
-        for (int v = 'a'; v <= 'z'; v++) found[src][v] = true;  
     }
 };

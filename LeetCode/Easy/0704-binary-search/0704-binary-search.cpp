f288c3f5a9ae9421d4949a163d08439e65296604
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2026-02-02 16:39:34 +0800

    Time: 0 ms (100%), Space: 31.3 MB (79.7%), Date: 02-02-2026

--- LeetCode/Easy/0704-binary-search/0704-binary-search.cpp
+++ LeetCode/Easy/0704-binary-search/0704-binary-search.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int left = 0;
-        int right = nums.size() - 1;
+        int l = 0, r = nums.size() - 1;
 
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] < target) left = mid + 1;
-            else if (nums[mid] > target) right = mid - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] < target) l++;
+            else if (nums[mid] > target) r--;
             else return mid;
         }
-        
+
         return -1;
     }
 };

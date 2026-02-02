181db9b4b4645942f09b1523e0203546a65e02c7
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2026-02-02 14:04:34 +0800

    Time: 10 ms (39.94%), Space: 15.9 MB (7.64%), Date: 02-02-2026

--- LeetCode/Easy/0001-two-sum/0001-two-sum.cpp
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.cpp
@@ -2,25 +2,21 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> res;
-        unordered_map<int, stack<int>> val2Pos;
+        unordered_map<int, int> um;
         
-        for (int i = 0; i < nums.size(); i++) val2Pos[nums[i]].push(i);
-
-        sort(nums.begin(), nums.end());
-
-        int i = 0;
-        int j = nums.size() - 1;
-        while (i < j) {
-            if (nums[i] + nums[j] > target) j--;
-            else if (nums[i] + nums[j] < target) i++;
-            else {
-                res.push_back(val2Pos[nums[i]].top());
-                val2Pos[nums[i]].pop();
-                res.push_back(val2Pos[nums[j]].top());
-                break;
-            }
+        for (int i = 0; i < nums.size(); i++) {
+            um[nums[i]] = i;
         }
-        
+
+        for (int i = 0; i < nums.size(); i++) {
+            int t = target - nums[i];
+            if (!um.count(t) || um[t] == i) continue;
+
+            res.push_back(i);
+            res.push_back(um[t]);
+            break;
+        }
+
         return res;
     }
 };

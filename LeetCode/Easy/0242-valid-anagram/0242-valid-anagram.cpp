ae3a74d35c6cdd7f9754d782608ff92574843624
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2026-02-02 15:18:18 +0800

    Time: 8 ms (7.3%), Space: 9.7 MB (48.19%), Date: 02-02-2026

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.cpp
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
         if (s.size() != t.size()) return false;
 
-        vector<int> st(26, 0);
-        vector<int> tt(26, 0);
-
         for (int i = 0; i < s.size(); i++) {
-            st[s[i] - 'a']++;
-            tt[t[i] - 'a']++;
+            if (s[i] != t[i]) return false;
         }
 
-        for (int i = 0; i < 26; i++) {
-            if (st[i] != tt[i]) return false;
-        }
-        
         return true;
     }
 };

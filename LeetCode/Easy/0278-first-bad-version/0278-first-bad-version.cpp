8a12386673e5fedaf031909fa916bf72ebccf144
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2026-02-03 11:57:47 +0800

    Time: 0 ms (100%), Space: 8 MB (40.96%), Date: 02-03-2026

--- LeetCode/Easy/0278-first-bad-version/0278-first-bad-version.cpp
+++ LeetCode/Easy/0278-first-bad-version/0278-first-bad-version.cpp
@@ -4,19 +4,14 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-        int left = 1;
-        int right = n;
-        int bad = n;
+        int l = 1, r = n;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (isBadVersion(mid)) {
-                if (mid < bad) bad = mid;
-                right = mid - 1;
-            }
-            else left = mid + 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (isBadVersion(mid)) r = mid - 1;
+            else l = mid + 1; 
         }
 
-        return bad;
+        return l;
     }
 };

708079cf1321e19d51b5538cf5ac6bfb8bb482df
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2026-02-03 12:04:20 +0800

    Time: 0 ms (100%), Space: 7.9 MB (70.7%), Date: 02-03-2026

--- LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.cpp
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> dp(n + 2, 0);
+        vector<int> dp(46, 0);
+    
         dp[1] = 1;
         dp[2] = 2;
-        for(int i = 3; i <= n; i++) {
-            dp[i] += dp[i - 1] + dp[i - 2];
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
+
         return dp[n];
     }
 };

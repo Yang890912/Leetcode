fa0de6e278ca124896456320c887d6a57366e15b
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2026-02-03 12:13:47 +0800

    Time: 0 ms (100%), Space: 9.1 MB (82.73%), Date: 02-03-2026

--- LeetCode/Easy/0409-longest-palindrome/0409-longest-palindrome.cpp
+++ LeetCode/Easy/0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        vector<int> lowNum(26, 0);
-        vector<int> upNum(26, 0);
+        vector<int> freq('z' + 1, 0);
         int res = 0;
-        int bonus = 0;
 
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] >= 'a' && s[i] <= 'z') lowNum[s[i] - 'a']++;
-            else if (s[i] >= 'A' && s[i] <= 'Z') upNum[s[i] - 'A']++;
-        }
-
-        for (int i = 0; i < 26; i++) {
-            res += lowNum[i] / 2 + upNum[i] / 2;
-            if (bonus == 0) {
-                bonus = max(bonus, lowNum[i] % 2);
-                bonus = max(bonus, upNum[i] % 2);
+        for (char c : s) {
+            freq[c]++;
+            if (freq[c] == 2) {
+                res += 2;
+                freq[c] = 0;
             }
         }
-        
-        return 2 * res + bonus;
+
+        for (char c : s) {
+            if (freq[c] == 1) {
+                res++;
+                break;
+            }
+        }
+
+        return res;
     }
 };

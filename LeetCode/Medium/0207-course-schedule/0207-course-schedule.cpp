51eaff526e0f88fe1f6f82446273402e39408825
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2026-02-04 19:33:17 +0800

    Time: 7 ms (48.21%), Space: 19.3 MB (68.46%), Date: 02-04-2026

--- LeetCode/Medium/0207-course-schedule/0207-course-schedule.cpp
+++ LeetCode/Medium/0207-course-schedule/0207-course-schedule.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adjList(numCourses);
-        vector<int> visited(numCourses, 0);
+        vector<int> visited(numCourses, -1);
+        vector<vector<int>> needed(numCourses);
 
-        for (int i = 0; i < prerequisites.size(); i++) {
-            adjList[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        for (vector<int>& pre : prerequisites) {
+            needed[pre[0]].push_back(pre[1]);
         }
 
         for (int i = 0; i < numCourses; i++) {
-            if (visited[i] != 0) continue;
-            visited[i] = 1;
-            if (!dfs(adjList, visited, i)) return false;
-            visited[i] = 2;
+            if (visited[i] == 1) continue;
+            if (!dfs(visited, needed, i)) return false;
         }
+
         return true;
     }
 
-    bool dfs(vector<vector<int>>& adjList, vector<int>& visited, int node) {
-        for (int i = 0; i < adjList[node].size(); i++) {
-            int neighbor = adjList[node][i];
-            // cout << neighbor << " " << visited[neighbor] << endl;
-            if (visited[neighbor] == 1) return false;
-            else if (visited[neighbor] == 2) continue;
-            else {
-                visited[neighbor] = 1;
-                if (!dfs(adjList, visited, neighbor)) return false;
-                visited[neighbor] = 2;
-            }
+    bool dfs(vector<int>& visited, vector<vector<int>>& needed, int curr) {
+        if (visited[curr] == 0) return false;
+        if (visited[curr] == 1) return true;
+
+        visited[curr] = 0;
+        for (int pre : needed[curr]) {
+            if (!dfs(visited, needed, pre)) return false;
         }
+        visited[curr] = 1;
+
         return true;
     }
 };

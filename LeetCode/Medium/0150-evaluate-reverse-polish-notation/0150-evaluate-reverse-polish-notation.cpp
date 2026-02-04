c08d4ebf645a30ca7d6a662d1b6f3dcb7788c19d
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2026-02-04 14:56:17 +0800

    Time: 3 ms (38.63%), Space: 17.2 MB (38.04%), Date: 02-04-2026

--- LeetCode/Medium/0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ LeetCode/Medium/0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -2,39 +2,30 @@ class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
         stack<int> nums;
-        for (int i = 0; i < tokens.size(); i++) {
-            if (tokens[i] == "+") {
-                int right = nums.top();
-                nums.pop();
-                int left = nums.top();
-                nums.pop();
-                nums.push(left + right);
-            }
-            else if (tokens[i] == "-") {
-                int right = nums.top();
-                nums.pop();
-                int left = nums.top();
-                nums.pop();
-                nums.push(left - right);
-            }
-            else if (tokens[i] == "*") {
-                int right = nums.top();
-                nums.pop();
-                int left = nums.top();
-                nums.pop();
-                nums.push(left * right);
-            }
-            else if (tokens[i] == "/") {
-                int right = nums.top();
-                nums.pop();
-                int left = nums.top();
-                nums.pop();
-                nums.push(left / right);
+
+        for (string s : tokens) {
+            if (isInteger(s)) {
+                int num = stoi(s);
+                nums.push(num);
             }
             else {
-                nums.push(stoi(tokens[i]));
+                int num2 = nums.top();
+                nums.pop();
+                int num1 = nums.top();
+                nums.pop();
+                if (s[0]== '+') nums.push(num1 + num2);
+                else if (s[0] == '-') nums.push(num1 - num2);
+                else if (s[0] == '*') nums.push(num1 * num2);
+                else if (s[0] == '/') nums.push(num1 / num2);
             }
         }
+
         return nums.top();
+    }
+
+    bool isInteger(string s) {
+        if (s.size() > 1) return true;
+        if (s[0] == '+' || s[0] == '-' || s[0] == '*' || s[0] == '/') return false;
+        return true;
     }
 };

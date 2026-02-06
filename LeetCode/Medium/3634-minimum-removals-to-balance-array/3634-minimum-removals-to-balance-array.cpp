183be5b0f67d7bc019653ff94db44ac5c1f9962c
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2026-02-06 10:11:03 +0800

    Time: 37 ms (38.08%), Space: 105 MB (86.63%), Date: 02-06-2026

---
+++ LeetCode/Medium/3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minRemoval(vector<int>& nums, int k) {
+        int n = nums.size();
+        int res = n;
+
+        if (n == 1) return 0;
+        sort(nums.begin(), nums.end());
+
+        int l = 0, r = 0;
+        while (l < n && r < n) {
+            if (nums[r] <= 1LL * k * nums[l]) {
+                res = min(res, n - (r - l + 1));
+                r++;
+            }
+            else {
+                l++;
+                r = max(r, l);
+            }
+        }
+        
+        return res;
+    }
+};

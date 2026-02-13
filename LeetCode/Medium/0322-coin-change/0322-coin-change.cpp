ac4fd13c470e37c3c4cefc105fc3ea8d8eee7c02
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2026-02-13 23:58:46 +0800

    Time: 23 ms (69.28%), Space: 22 MB (48.2%), Date: 02-13-2026

--- LeetCode/Medium/0322-coin-change/0322-coin-change.cpp
+++ LeetCode/Medium/0322-coin-change/0322-coin-change.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> dp(amount + 1, INT_MAX - 1);
+        vector<long long> dp(amount + 1, INT_MAX);
+        int n = coins.size();
+
         dp[0] = 0;
         for (int i = 1; i <= amount; i++) {
-            for (int j = 0; j < coins.size(); j++) {
-                if (i < coins[j]) continue;
-
-                if (dp[i] > dp[i - coins[j]] + 1) dp[i] = dp[i - coins[j]] + 1;
+            for (int j = 0; j < n; j++) {
+                if (coins[j] > i) continue;
+                dp[i] = min(dp[i], dp[i - coins[j]] + 1);
             }
         }
-        return dp[amount] == INT_MAX - 1 ? -1 : dp[amount];
+
+        if (dp[amount] == INT_MAX) return -1;
+        
+        return dp[amount];
     }
 };

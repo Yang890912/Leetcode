22b2cb30d216cebdd522afc354958b1db2522ba6
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2026-02-15 01:18:48 +0800

    Time: 0 ms (100%), Space: 15.1 MB (95.44%), Date: 02-15-2026

--- LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ LeetCode/Medium/0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int left = 0;
-        int right = nums.size() - 1;
+        int l = 0;
+        int r = nums.size() - 1;
 
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            
+        while (l <= r) {
+            int mid = (l + r) / 2;
             if (nums[mid] == target) return mid;
-
-            if (nums[left] <= nums[mid]) {   // left to mid is sorted
-                if (nums[left] <= target && target <= nums[mid]) right = mid - 1;
-                else left = mid + 1;
+            else if (nums[l] <= nums[mid]) {
+                if (nums[l] <= target && target < nums[mid]) r = mid - 1;
+                else l = mid + 1;
             }
-            else {  // mid to right is sorted
-                if (nums[mid] <= target && target <= nums[right]) left = mid + 1;
-                else right = mid - 1;
+            else {
+                if (nums[mid] < target && target <= nums[r]) l = mid + 1;
+                else r = mid - 1;
             }
         }
 

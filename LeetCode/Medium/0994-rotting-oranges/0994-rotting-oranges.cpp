150f6a4d9ec9c7d3c3f277718ba1d10a24e54b2f
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2026-02-14 17:14:51 +0800

    Time: 0 ms (100%), Space: 17.6 MB (9.77%), Date: 02-14-2026

--- LeetCode/Medium/0994-rotting-oranges/0994-rotting-oranges.cpp
+++ LeetCode/Medium/0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,43 +1,50 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        int minutes = 0;
-        queue<vector<int>> q;
-        vector<pair<int, int>> DIR = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        const int m = grid.size();
+        const int n = grid[0].size();
+        const vector<vector<int>> DIR = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        queue<pair<int, int>> q;
+        int numOrange = 0;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 2) 
-                    q.push({i, j, 0});
+                if (grid[i][j] == 2) {
+                    q.push({i, j});
+                    visited[i][j] = true;
+                }
+                else if (grid[i][j] == 1) numOrange++;
             }
         }
 
-        while (!q.empty()) {
-            int x = q.front()[0];
-            int y = q.front()[1];
-            int level = q.front()[2];
-            
-            q.pop();
-            if (level >= minutes) minutes = level;
+        if (numOrange == 0) return 0;
 
-            for (int i = 0; i < DIR.size(); i++) {
-                int nx = x + DIR[i].first;
-                int ny = y + DIR[i].second;
-                if (nx < m && nx >= 0 && ny < n && ny >= 0 && grid[nx][ny] == 1) {
-                    grid[nx][ny] = 2;
-                    q.push({nx, ny, level + 1});
+        int res = -1;
+        while (!q.empty()) {
+            int size = q.size();
+            res++;
+
+            while (size--) {
+                int x = q.front().first;
+                int y = q.front().second;
+                q.pop();
+
+                for (auto& d : DIR) {
+                    int nx = x + d[0];
+                    int ny = y + d[1];
+
+                    if (nx < 0 || ny < 0 || nx >= m || ny >= n || visited[nx][ny] || grid[nx][ny] != 1) continue;
+
+                    numOrange--;
+                    visited[nx][ny] = true;
+                    q.push({nx, ny});
                 }
             }
         }
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1) return -1;
-            }
-        }
+        if (numOrange > 0) return -1;
 
-        return minutes;
+        return res;
     }
 };

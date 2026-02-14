904d06f556efff3e44f405d5d987bcc3de23e85f
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2026-02-14 16:35:46 +0800

    Time: 25 ms (71.68%), Space: 16.9 MB (53.62%), Date: 02-14-2026

--- LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.cpp
+++ LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
+    const vector<vector<int>> DIR = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        int numOfIslands = 0;
+        int res = 0;
         vector<vector<bool>> visited(m, vector<bool>(n, false));
-        
+
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1' && !visited[i][j]) {
-                    dfs(grid, visited, i, j);
-                    numOfIslands++;
-                }
+                if (visited[i][j] || grid[i][j] == '0') continue;
+                dfs(grid, visited, m, n, i, j);
+                res++;
             }
         }
-        
-        return numOfIslands;
+
+        return res;
     }
 
-    void dfs(vector<vector<char>>& grid, vector<vector<bool>>& visited, int x, int y) {
+    void dfs(vector<vector<char>>& grid, vector<vector<bool>>& visited, int m, int n, int x, int y) {
         visited[x][y] = true;
-        if (x - 1 >= 0 && grid[x - 1][y] == '1' && !visited[x - 1][y]) 
-            dfs(grid, visited, x - 1, y);
-        if (x + 1 < grid.size() && grid[x + 1][y] == '1' && !visited[x + 1][y]) 
-            dfs(grid, visited, x + 1, y);
-        if (y - 1 >= 0 && grid[x][y - 1] == '1' && !visited[x][y - 1]) 
-            dfs(grid, visited, x, y - 1);
-        if (y + 1 < grid[0].size() && grid[x][y + 1] == '1' && !visited[x][y + 1]) 
-            dfs(grid, visited, x, y + 1);
+
+        for (auto& d : DIR) {
+            int nx = x + d[0];
+            int ny = y + d[1];
+            if (nx < 0 || ny < 0 || nx >= m || ny >= n || visited[nx][ny] || grid[nx][ny] == '0') continue;
+            dfs(grid, visited, m, n, nx, ny);
+        }
     }
 };

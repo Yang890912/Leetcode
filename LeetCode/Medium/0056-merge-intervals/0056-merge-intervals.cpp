fe4b19013ac3badf2c1d944016034b5e69305e73
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2026-02-16 20:35:41 +0800

    Time: 3 ms (89.19%), Space: 23.7 MB (80.64%), Date: 02-16-2026

--- LeetCode/Medium/0056-merge-intervals/0056-merge-intervals.cpp
+++ LeetCode/Medium/0056-merge-intervals/0056-merge-intervals.cpp
@@ -2,23 +2,21 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         sort(intervals.begin(), intervals.end());
+        int n = intervals.size();
         vector<vector<int>> res;
-        int left = intervals[0][0];
-        int right = intervals[0][1];
+        vector<int> curr = intervals[0];
 
-        for (int i = 1; i < intervals.size(); i++) {
-            if (intervals[i][0] <= right) {
-                right = max(right, intervals[i][1]);
-                continue;
+        for (int i = 1; i < n; i++) {
+            if (curr[1] < intervals[i][0]) {
+                res.push_back(curr);
+                curr = intervals[i];
             }
             else {
-                res.push_back({left, right});
-                left = intervals[i][0];
-                right = intervals[i][1];
+                curr[1] = max(curr[1], intervals[i][1]);
             }
         }
 
-        res.push_back({left, right});
+        if (curr.size() != 0) res.push_back(curr);
 
         return res;
     }

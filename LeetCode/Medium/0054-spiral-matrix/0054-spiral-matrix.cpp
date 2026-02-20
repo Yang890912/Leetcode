6d8d108739748ed285617d2d505bdbe5981838be
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2026-02-20 22:54:32 +0800

    Time: 0 ms (100%), Space: 9.5 MB (14.2%), Date: 02-20-2026

--- LeetCode/Medium/0054-spiral-matrix/0054-spiral-matrix.cpp
+++ LeetCode/Medium/0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,37 +1,32 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-
+        const vector<vector<int>> DIR = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        const int m = matrix.size();
+        const int n = matrix[0].size();
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
         vector<int> res;
-        int left = 0, right = n - 1, top = 0, down = m - 1;
-
-        while (top <= down && left <= right) {
-            for (int i = left; i <= right; i++) {
-                res.push_back(matrix[top][i]);
-            }
-            top++;
-
-            for (int i = top; i <= down; i++) {
-                res.push_back(matrix[i][right]);
-            }
-            right--;
-
-            if (top <= down) {
-                for (int i = right; i >= left; i--) {
-                    res.push_back(matrix[down][i]);
-                }   
-                down--;
+        
+        int x = 0, y = 0, turn = 0;
+        while (true) {
+            res.push_back(matrix[x][y]);
+            visited[x][y] = true;
+            
+            int nx = x + DIR[turn][0];
+            int ny = y + DIR[turn][1];
+            if (nx < 0 || ny < 0 || nx >= m || ny >= n || visited[nx][ny]) {
+                turn++;
+                turn %= 4;
+                nx = x + DIR[turn][0];
+                ny = y + DIR[turn][1];
             }
 
-            if (left <= right) {
-                for (int i = down; i >= top; i--) {
-                    res.push_back(matrix[i][left]);
-                }
-                left++;
-            }
+            if (nx < 0 || ny < 0 || nx >= m || ny >= n || visited[nx][ny]) break;
+
+            x = nx;
+            y = ny;
         }
+
         return res;
     }
 };

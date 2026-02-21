08f32b603fbd84dee2c440a58c073bff0dd47387
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2026-02-21 14:19:20 +0800

    Time: 270 ms (54.31%), Space: 10.8 MB (55.13%), Date: 02-21-2026

--- LeetCode/Medium/0079-word-search/0079-word-search.cpp
+++ LeetCode/Medium/0079-word-search/0079-word-search.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
-    bool exist(vector<vector<char>>& board, string word) {
-        vector<vector<int>> visited(board.size(), vector<int>(board[0].size(), -1));
+    const vector<vector<int>> DIR = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
-        for (int i = 0; i < board.size(); i++) {
-            for (int j = 0; j < board[0].size(); j++) {
-                if(dfs(board, i, j, visited, word, 0)) 
-                    return true;
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size();
+        int n = board[0].size();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] != word[0]) continue;
+
+                vector<vector<bool>> visited(m, vector<bool>(n, false));
+                if (dfs(board, i, j, visited, word, 0)) return true;
             }
         }
 
         return false;
     }
 
-    bool dfs(const vector<vector<char>>& board, 
-            int i, int j,
-            vector<vector<int>>& visited, 
-            string word, 
-            int idx) {
+    bool dfs(vector<vector<char>>& board, int x, int y, vector<vector<bool>>& visited, string& word, int pos) {
+        visited[x][y] = true;
         
-        if (idx == word.size()) 
-            return true;
+        if (pos == word.size() - 1) return true;
 
-        if (i < 0 || i >= board.size() || 
-            j < 0 || j >= board[0].size() ||
-            board[i][j] != word[idx] || 
-            visited[i][j] == 1) 
-            return false;
+        for (auto& d : DIR) {
+            int nx = x + d[0];
+            int ny = y + d[1];
 
-        visited[i][j] = 1;
-        bool found =    dfs(board, i + 1, j, visited, word, idx + 1) ||
-                        dfs(board, i - 1, j, visited, word, idx + 1) ||
-                        dfs(board, i, j + 1, visited, word, idx + 1) ||
-                        dfs(board, i, j - 1, visited, word, idx + 1);
-        visited[i][j] = -1;
+            if (nx < 0 || ny < 0 || nx >= board.size() || ny >= board[0].size() 
+            || visited[nx][ny] || board[nx][ny] != word[pos + 1]) continue;
 
-        return found;
+            if (dfs(board, nx, ny, visited, word, pos + 1)) return true;
+
+            visited[nx][ny] = false;
+        }
+
+        return false;
     }
-};
+};  

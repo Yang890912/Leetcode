50ce707c9ec5b17ac0ca0d3126b5f15d44ec3fb0
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2026-02-21 13:50:21 +0800

    Time: 27 ms (23.88%), Space: 8.4 MB (7.9%), Date: 02-21-2026

---
+++ LeetCode/Easy/0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countPrimeSetBits(int left, int right) {
+        unordered_set<int> primes = {2, 3, 5, 7, 11, 13, 17, 19};
+        int res = 0;
+
+        for (int i = left; i <= right; i++) {
+            if (primes.count(bitCounts(i))) res++;
+        }
+
+        return res;
+    }
+
+    int bitCounts(int num) {
+        int count = 0;
+        while (num) {
+            if (num & 1) count++;
+            num >>= 1;
+        }
+        return count;
+    }
+};

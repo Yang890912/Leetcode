f6fa412d6018464cdb5e0569e35530bb8b1c63b0
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2026-02-28 15:40:09 +0800

    Time: 335 ms (27.52%), Space: 8.1 MB (69.8%), Date: 02-28-2026

---
+++ LeetCode/Medium/1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int concatenatedBinary(int n) {
+        const int MOD = 1e9 + 7;
+        long long res = 0;
+        for (int i = 1; i <= n; i++) {
+            int num = i;
+            while (num) {
+                num >>= 1;
+                res = (res << 1) % MOD;
+            }
+            res = (res + i) % MOD;
+        }
+
+        return res;
+    }
+};

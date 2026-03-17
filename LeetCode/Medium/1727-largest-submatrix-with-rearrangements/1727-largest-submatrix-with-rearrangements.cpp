90a365abb1fd3e8af78e96e0dcea82dedc5829fd
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2026-03-17 20:00:06 +0800

    Time: N/A (0%), Space: N/A (0%), Date: 03-17-2026

--- LeetCode/Medium/1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
+++ LeetCode/Medium/1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
@@ -4,22 +4,18 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
         int res = 0;
-        vector<vector<int>> cum(m, vector<int>(n, 0));
 
         for (int j = 0; j < n; j++) {
-            int sum = 0;
-            for (int i = 0; i < m; i++) {
-                if (matrix[i][j] == 0) sum = 0;
-                else sum++;
-                cum[i][j] = sum;
+            for (int i = 1; i < m; i++) {
+                if (matrix[i][j] == 1) matrix[i][j] = matrix[i - 1][j] + 1;
             }
         }
 
         for (int i = 0; i < m; i++) {
-            sort(cum[i].rbegin(), cum[i].rend());
+            sort(matrix[i].rbegin(), matrix[i].rend());
             for (int j = 0; j < n; j++) {
-                if (cum[i][j] == 0) break;
-                res = max(res, cum[i][j] * (j + 1));
+                if (matrix[i][j] == 0) break;
+                res = max(res, matrix[i][j] * (j + 1));
             }
         }
 

98ef0d156b928a6214758e1dcb6717d7ba97c2b0
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2026-03-26 16:48:29 +0800

    Time: N/A (0%), Space: N/A (0%), Date: 03-26-2026

--- LeetCode/Medium/3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.cpp
+++ LeetCode/Medium/3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.cpp
@@ -3,13 +3,13 @@ public:
     bool canPartitionGrid(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        long long sum = 0;
+        long long total = 0;
         vector<long long> rowSum(m, 0);
         vector<long long> colSum(n, 0);
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                sum += grid[i][j];
+                total += grid[i][j];
                 rowSum[i] += grid[i][j];
                 colSum[j] += grid[i][j];
             }
@@ -18,14 +18,14 @@ public:
         long long tmp = 0;
         for (int i = 0; i < m; i++) {
             tmp += rowSum[i];
-            if (tmp * 2 == sum) return true;
-            else if (tmp * 2 > sum) break;
+            if (tmp * 2 == total) return true;
+            else if (tmp * 2 > total) break;
         }
         tmp = 0;
         for (int j = 0; j < n; j++) {
             tmp += colSum[j];
-            if (tmp * 2 == sum) return true;
-            else if (tmp * 2 > sum) break;
+            if (tmp * 2 == total) return true;
+            else if (tmp * 2 > total) break;
         }
 
         return false;

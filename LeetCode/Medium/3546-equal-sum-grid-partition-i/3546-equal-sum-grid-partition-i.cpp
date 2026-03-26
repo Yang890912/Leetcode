f7829574eae22ee38ac275b61344702e82c89eb0
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2026-03-26 16:53:57 +0800

    Time: N/A (0%), Space: N/A (0%), Date: 03-26-2026

--- LeetCode/Medium/3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.cpp
+++ LeetCode/Medium/3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.cpp
@@ -4,28 +4,25 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         long long total = 0;
-        vector<long long> rowSum(m, 0);
-        vector<long long> colSum(n, 0);
+        vector<vector<long long>> prefixSum(m, vector<long long>(n, 0));
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 total += grid[i][j];
-                rowSum[i] += grid[i][j];
-                colSum[j] += grid[i][j];
+                if (i == 0 && j == 0) prefixSum[i][j] = grid[i][j];
+                else if (i == 0) prefixSum[i][j] = grid[i][j] + prefixSum[i][j - 1];
+                else if (j == 0) prefixSum[i][j] = grid[i][j] + prefixSum[i - 1][j];
+                else prefixSum[i][j] = grid[i][j] + prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1];
             }
         }
 
-        long long tmp = 0;
         for (int i = 0; i < m; i++) {
-            tmp += rowSum[i];
-            if (tmp * 2 == total) return true;
-            else if (tmp * 2 > total) break;
+            if (total == prefixSum[i][n - 1] * 2) return true;
+            if (total > prefixSum[i][n - 1] * 2) break;
         }
-        tmp = 0;
         for (int j = 0; j < n; j++) {
-            tmp += colSum[j];
-            if (tmp * 2 == total) return true;
-            else if (tmp * 2 > total) break;
+            if (total == prefixSum[n - 1][j] * 2) return true;
+            if (total > prefixSum[n - 1][j] * 2) break;
         }
 
         return false;

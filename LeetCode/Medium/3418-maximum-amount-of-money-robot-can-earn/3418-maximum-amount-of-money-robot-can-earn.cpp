ede000dd498ef291902a5c8f5f59e9ef6570927e
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2026-04-02 16:51:13 +0800

    Time: 547 ms (53.19%), Space: 276 MB (46.81%), Date: 04-02-2026

---
+++ LeetCode/Medium/3418-maximum-amount-of-money-robot-can-earn/3418-maximum-amount-of-money-robot-can-earn.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maximumAmount(vector<vector<int>>& coins) {
+        int m = coins.size();
+        int n = coins[0].size();
+        vector<vector<vector<int>>> dp(m, vector(n, vector<int>(3, INT_MIN)));
+        dp[0][0][1] = dp[0][0][2] = 0;
+        dp[0][0][0] = coins[0][0];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < 3; k++) {
+                    if (i) dp[i][j][k] = max(dp[i][j][k], dp[i - 1][j][k] + coins[i][j]);
+                    if (i && k) dp[i][j][k] = max(dp[i][j][k], dp[i - 1][j][k - 1]);
+                    if (j) dp[i][j][k] = max(dp[i][j][k], dp[i][j - 1][k] + coins[i][j]);
+                    if (j && k) dp[i][j][k] = max(dp[i][j][k], dp[i][j - 1][k - 1]);
+                }
+            }
+        }
+
+        return max(max(dp[m - 1][n - 1][0], dp[m - 1][n - 1][1]), dp[m - 1][n - 1][2]);
+    }
+};

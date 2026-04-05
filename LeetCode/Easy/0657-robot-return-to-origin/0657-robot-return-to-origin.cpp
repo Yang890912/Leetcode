a8053df2f47313024515886b9be9cbd5d065f75b
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2026-04-05 13:32:45 +0800

    Time: N/A (0%), Space: N/A (0%), Date: 04-05-2026

--- LeetCode/Easy/0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
+++ LeetCode/Easy/0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -3,10 +3,10 @@ public:
     bool judgeCircle(string moves) {
         int x = 0, y = 0;
         for (char c : moves) {
-            if (c == 'L') x -= 1;
-            else if (c == 'R') x += 1;
-            else if (c == 'U') y += 1;
-            else if (c == 'D') y -= 1;
+            if (c == 'L') x--;
+            else if (c == 'R') x++;
+            else if (c == 'U') y++;
+            else if (c == 'D') y--;
         }
         return (x == 0 && y == 0);
     }

1a80e65bc17c7af7a6bde567356ac77739bfc08a
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2026-04-11 13:10:53 +0800

    Time: N/A (0%), Space: N/A (0%), Date: 04-11-2026

--- LeetCode/Medium/3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.cpp
+++ LeetCode/Medium/3741-minimum-distance-between-three-equal-elements-ii/3741-minimum-distance-between-three-equal-elements-ii.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int minimumDistance(vector<int>& nums) {
-        vector<vector<int>> freq(100001, vector<int>());
-        for (int i = 0; i < nums.size(); i++) {
-            freq[nums[i]].push_back(i);    
+        map<int, vector<int>> mp;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            mp[nums[i]].push_back(i);    
         }
 
         int res = INT_MAX;
-        for (int i = 1; i < 100001; i++) {
-            if (freq[i].size() < 3) continue;
-            for (int j = 0; j < freq[i].size() - 2; j++) {
-                res = min(res, 
-                        abs(freq[i][j] - freq[i][j + 1]) + abs(freq[i][j + 1] - freq[i][j + 2]) + abs(freq[i][j] - freq[i][j + 2]));
+        for (auto& it : mp) {
+            vector<int> v = it.second;
+            if (v.size() < 3) continue;
+            for (int j = 0; j < v.size() - 2; j++) {
+                res = min(res, 2 * abs(v[j] - v[j + 2]));
             }
         }
 

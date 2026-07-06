4e48727f3561dc5829ce094a932883ab1719374c
Yang890912 <90774036+Yang890912@users.noreply.github.com>
2026-07-06 11:13:02 +0800

    Time: 74 ms (6.49%), Space: 33.6 MB (5.3%), Date: 07-06-2026

--- LeetCode/Medium/1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ LeetCode/Medium/1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -10,13 +10,9 @@ public:
         int res = intervals.size();
         int i = 0, j = 1;
         while (i < n && j < n) {
-            if (intervals[i][0] <= intervals[j][0] 
-                && intervals[j][1] <= intervals[i][1]) {
-                res--;
-                j++;
-            }
-            else if (i < j - 1) i++;
-            else if (i == j - 1) j++;
+            if (intervals[i][0] <= intervals[j][0] && intervals[j][1] <= intervals[i][1]) res--;
+            else i = j;
+            j++;
         }
 
         return res;
